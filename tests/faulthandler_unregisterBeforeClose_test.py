d63cf4ef3ddbe85dcf939903c8c0813ef9a239ef
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-05-01 18:30:11 +0300

    add tests for faulthandler_unregisterBeforeClose

---
+++ tests/faulthandler_unregisterBeforeClose_test.py
@@ -0,0 +1,56 @@
+import faulthandler
+import signal
+from nltk.tokenize import util
+import sys
+import io
+
+def test_ok_1():
+    # Register a handler for SIGUSR1 to dump the current thread's traceback
+    faulthandler.register(signum=signal.SIGUSR1, all_threads=False)
+    faulthandler.unregister(signal.SIGUSR1)
+
+def test_ok_2():
+    # Setup the environment
+    traceback_file_path = "traceback.log"
+
+    # Open a file to write the traceback information
+    f = open(traceback_file_path, "w")
+
+    # Enable the fault handler with the specified file
+    faulthandler.enable(file=f)
+
+    # Register a handler for SIGUSR1 to dump the current thread's traceback
+    faulthandler.register(signal.SIGUSR1, file=f, all_threads=False)
+
+    faulthandler.unregister(signal.SIGUSR1)
+
+    # correct usage: closing after unregister
+    f.close()
+
+
+def test_violation_1():
+    # Register a handler for SIGUSR1 to dump the current thread's traceback
+    faulthandler.register(signum=signal.SIGUSR1, all_threads=False)
+
+    # misuse: closing before unregister
+    sys.stderr.close()
+
+    faulthandler.unregister(signal.SIGUSR1)
+
+def test_violation_2():
+    # Setup the environment
+    traceback_file_path = "traceback.log"
+
+    # Open a file to write the traceback information
+    f = open(traceback_file_path, "w")
+
+    # Enable the fault handler with the specified file
+    faulthandler.enable(file=f)
+
+    # Register a handler for SIGUSR1 to dump the current thread's traceback
+    faulthandler.register(signal.SIGUSR1, file=f, all_threads=False)
+
+    # misuse: closing before unregister
+    f.close()
+
+    faulthandler.unregister(signal.SIGUSR1)
