9c779c83fc72daa26148248984a4fdd2eeab0a49
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-05-08 22:13:17 +0300

    test: add tests for print side effect violation for TfFunction_NoSideEffect spec

--- tests/TfFunction_NoSideEffect_test.py
+++ tests/TfFunction_NoSideEffect_test.py
@@ -14,6 +14,18 @@ def test_ok_2():
     print(append_to_list(tf.constant(3)))
     print(append_to_list(tf.constant(2.0)))
 
+def test_ok_3():
+    @tf.function
+    def append_to_list(x):
+        return x * x
+
+    # Testing the monkey-patched tf.function
+    print(append_to_list(tf.constant(2)))
+    print(append_to_list(tf.constant(3)))
+    print(append_to_list(tf.constant(2.0)))
+
+    print('hi')
+
 def test_violation_1():
     @tf.function
     def append_to_list(x):
@@ -23,3 +35,12 @@ def test_violation_1():
 
     # Testing the monkey-patched tf.function
     print(append_to_list(tf.constant(2)))
+
+def test_violation_2():
+    @tf.function
+    def append_to_list(x):
+        print('hi') # violation (side effect in a tf.function)
+        return x * x
+
+    # Testing the monkey-patched tf.function
+    append_to_list(tf.constant(2))
