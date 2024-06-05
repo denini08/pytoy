00712f619a45d59b2fb1dbb84d4bac5c0ece18a1
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-06-05 13:57:20 +0300

    add tests for XMLParser_ParseMustFinalize spec

---
+++ tests/XMLParser_ParseMustFinalize_test.py
@@ -0,0 +1,32 @@
+import xml.parsers.expat
+
+def test_ok_1():
+    # Create an XML parser object
+    p = xml.parsers.expat.ParserCreate()
+
+    # Parse the XML document in chunks
+    p.Parse('<root>', False)  # Parsing first chunk
+    p.Parse('<child>data</child>', False)  # Parsing second chunk
+    p.Parse('</root>', True)  # CORRECT USAGE: Parsing final chunk and correctly indicating it's the end
+
+def test_ok_2():
+    p = xml.parsers.expat.ParserCreate()
+    p.Parse('<root>', False)
+    p.Parse('<child>data</child>', False) 
+    # This will throw an error because the XML is invalid and this is the last chunk
+    p.Parse('<root>', True)
+
+def test_violation_1():
+    p = xml.parsers.expat.ParserCreate()
+    p.Parse('<root>', False)
+    p.Parse('<child>data</child>', False) 
+    p.Parse('</root>', False) # Violation: not indicating that it is the final chunk
+
+def test_violation_2():
+    p = xml.parsers.expat.ParserCreate()
+    p.Parse('<root>', False)
+    p.Parse('<child>data</child>', False) 
+    # Note that the XML is invalid because here we need
+    # </root> instead of <root> however, it is not checked
+    # because is_final = False
+    p.Parse('<root>', False)
